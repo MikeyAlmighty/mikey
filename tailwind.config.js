75b40863eceb21dd3c167a2a9cfdb9812c8cabe8
Mikey <45830554+MikeyAlmighty@users.noreply.github.com>
2022-12-12 10:48:20 +0200

    updated content resolution in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ module.exports = {
   content: [
     "./renderer/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}"
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./assets/pages/**/*.{js,ts,jsx,tsx}",
+    "./assets/renderer/**/*.{js,ts,jsx,tsx}",
+    "./assets/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
